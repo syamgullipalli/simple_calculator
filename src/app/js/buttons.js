f26c40c29b4a9291725bfbe6abf354e55fc55092
Syam Gullipalli <mail2ajaysimha@gmail.com>
2016-09-01 12:16:37 +0200

    Dev (BL): - define tasks as TODOs

---
+++ src/app/js/buttons.js
@@ -0,0 +1,3 @@
+//TODO On click number and operator: display on query screen
+//TODO On click clear: clear the the query and result
+//TODO If result screen is not cleared (not 0) and clicked on operator: precede the query screen with result
---
+++ src/app/js/evaluate.js
@@ -0,0 +1,9 @@
+//TODO Only 22 characters are allowed on query and result screen
+//TODO Suppress the result to 22 characters if longer than 22 characters
+//TODO Only one dot allowed per on result screen
+//TODO Only one dot allowed per number (One dot allowed in between two operators)
+//TODO Only - operator is allowed on the beginning of the query
+//TODO Two operators can't fit fit consecutively
+//TODO Check for matching pairs of braces ( )
+//TODO Parse the query string and modify if required
+//TODO Divided by zero: exception handle
